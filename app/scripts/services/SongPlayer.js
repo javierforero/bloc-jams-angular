810553febbc80110dff28b724a512ec7318ee165
Javier Forero <javierforero01@gmail.com>
2016-08-29 08:00:43 -0500

    checkpoint-10 not finished

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -74,6 +74,11 @@
      */
      SongPlayer.currentSong = null;
      /*
+     * @desc Current playback time (in seconds) of currently playing song
+     * @type {Number}
+     */
+     SongPlayer.currentTime = null;
+     /*
      * @function in SongPlayer object
      * @desc plays a specified song
      * @param {Object} song
@@ -138,6 +143,17 @@
            var song = currentAlbum.songs[currentSongIndex];
            setSong(song);
            playSong(song);
+         }
+     };
+
+     /*
+     * @function setCurrentTime
+     * @desc Set current time (in seconds) of currently playing song
+     * @param {Number} time
+     */
+     SongPlayer.setCurrentTime = function(time) {
+         if (currentBuzzObject) {
+             currentBuzzObject.setTime(time);
          }
      };
 
